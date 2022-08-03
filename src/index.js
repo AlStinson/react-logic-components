30441dc4a24f2750f9e87edeb065b530401dd155
Javier Delgado Cruces <javidelgado1997@gmail.com>
2022-08-03 21:18:50 +0200

    Fix index.js imports and build script and add push script

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import For from './For';
+import ForEach from './ForEach';
+
+export { For, ForEach };
 export { If, Else, ElseIf, IfBlock, Ifs } from './If';
-export { For } from './For';
 export { Switch, Case, Default } from './Switch';
-export { ForEach } from './ForEach';
